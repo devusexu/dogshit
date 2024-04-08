bf4d2fc59568757529a9beffb373285d3ff6703c
devusexu <devusexu@gmail.com>
2024-04-08 14:40:26 +0800

    refactor: move error handling logic to a separate file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const authRouter = require('./src/routes/auth');
 const taskRouter = require('./src/routes/task');
 const isAuthenticated = require('./src/middlewares/authenticator');
-const { CustomError } = require('./utils/error');
+const errorHandler = require('./src/middlewares/errorHandler');
 const app = express();
 
 // assume request body is json object 
@@ -28,36 +28,6 @@ async function main() {
 app.use('/api/users', authRouter);
 app.use('/api/tasks', isAuthenticated, taskRouter);
 
-app.use((err, req, res, next) => {
-    console.error(err);
-
-    if (err.isJoi) {
-        return res.status(422).json({
-            message: err.details[0].message
-        });
-    }
-
-    if (err instanceof CustomError) {
-        return res.status(err.statusCode).json({
-            message: err.message
-        })
-    }
-
-    if (err.name === 'CastError' && err.kind === 'ObjectId') {
-        // Handle CastError due to incorrect _id format
-        return res.status(400).json({ message: 'Invalid ObejctId format' });
-    }
-
-    if (err.code === 11000 && err.keyPattern && err.keyPattern.email === 1) {
-        // Duplicate email error (MongoDB unique constraint violation)
-        return res.status(409).json({
-            message: 'Email already exists'
-        });
-    }
-    
-    return res.status(500).json({
-        message: 'Internal Server Error'
-    });
-});
+app.use(errorHandler);
 
 app.listen(3000);
